60caefab1a09692ca90e00709878fd2863d76130
mcreader <mcreader215@gmail.com>
2019-04-18 15:43:01 +0300

    Change router to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 
 import CompaniesTablePage from './companies/ui/CompaniesTablePage';
 import CompanyDetailsPage from './companies/ui/CompanyDetailsPage';
