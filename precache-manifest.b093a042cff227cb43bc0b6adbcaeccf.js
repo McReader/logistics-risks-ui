0726548df68320ea5c93e0fe3b982fd56a677c52
mcreader <mcreader215@gmail.com>
2019-04-24 18:32:36 +0300

    Updates

---
+++ precache-manifest.b093a042cff227cb43bc0b6adbcaeccf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abf5785a05437c98a187",
+    "url": "/logistics-risks-ui/static/js/runtime~main.320d1aeb.js"
+  },
+  {
+    "revision": "a6d342e051edaef8ff4f",
+    "url": "/logistics-risks-ui/static/js/main.de2b28c1.chunk.js"
+  },
+  {
+    "revision": "64df0f7ef56a912abf2c",
+    "url": "/logistics-risks-ui/static/js/2.36d11c68.chunk.js"
+  },
+  {
+    "revision": "a6d342e051edaef8ff4f",
+    "url": "/logistics-risks-ui/static/css/main.f479bbbf.chunk.css"
+  },
+  {
+    "revision": "a28c1050a1d015a82fc3ec9fdc243d6f",
+    "url": "/logistics-risks-ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/logistics-risks-ui/precache-manifest.67130015bf7e5b62ecc31feeb63b2393.js"
+  "/logistics-risks-ui/precache-manifest.b093a042cff227cb43bc0b6adbcaeccf.js"
 );
 
 workbox.clientsClaim();
